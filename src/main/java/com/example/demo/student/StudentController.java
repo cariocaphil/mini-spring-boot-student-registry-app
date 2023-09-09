0ca01ffb01c3582bf82cb6f82307afa66ff1353f
cariocaphil <cariocaphil@gmail.com>
2023-09-09 11:12:05 +0200

    add service for initial n-tier architecture

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -6,24 +6,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 import com.example.demo.student.Student;
-import java.time.LocalDate;
-import java.time.Month.JANUARY;
+import com.example.demo.student.StudentService;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
+  private final StudentService studentService;
+
+  public StudentController(StudentService studentService){
+    this.studentService = studentService;
+  }
+
   @GetMapping
   public List<Student> getStudents() {
-    return List.of(
-        new Student(
-            1L,
-            "Mariam",
-            "mariam.jamal@gmail.com",
-            LocalDate.of(2000, Month.JANUARY, 5),
-            21
-        )
-    );
+    return studentService.getStudents();
   }
 }
 
