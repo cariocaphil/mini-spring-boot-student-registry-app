9ec60fb07545143f7e249c156d311bd7b5bedf1e
cariocaphil <cariocaphil@gmail.com>
2023-09-09 09:25:08 +0200

    List example

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,7 +15,7 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public String hello() {
-		return "Hello World";
+	public List<String> hello() {
+		return List.of("Hello", "World");
 	}
 }
