f4f1bff815fe67d0d08069616ac7635391823075
cariocaphil <cariocaphil@gmail.com>
2023-09-10 18:22:38 +0200

    add studentConfig and saveAll

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,20 @@
+public class StudentConfig {
+
+  @Bean
+  CommandLineRunner commandLineRunner(StudentRepository repository) {
+    return args -> {
+      Student mariam = new Student(
+          "Mariam",
+          "mariam.jamal@gmail.com",
+          LocalDate.of(2000, Month.JANUARY, 5),
+          21);
+      Student alex = new Student(
+          "Alex",
+          "alex@gmail.com",
+          LocalDate.of(2000, Month.JANUARY, 5),
+          21);
+
+      repository.saveAll(List.of(mariam, alex));
+    }
+  }
+}
