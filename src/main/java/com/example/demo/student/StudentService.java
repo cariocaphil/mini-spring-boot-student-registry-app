f9236356537579739ffef72ca0fb4a48c921ed50
cariocaphil <cariocaphil@gmail.com>
2023-09-10 18:02:31 +0200

    add studentRepository and findAll

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -9,15 +9,15 @@ import java.time.Month.JANUARY;
 @Service
 public class StudentService {
 
+  private final StudentRepository
+
+  @Autowired
+  public StudentService(StudentRepository studentRepository) {
+    this.studentRepository = studentRepository;
+  }
+
   public List<Student> getStudents() {
-    return List.of(
-        new Student(
-            1L,
-            "Mariam",
-            "mariam.jamal@gmail.com",
-            LocalDate.of(2000, Month.JANUARY, 5),
-            21)
-    );
+    return studentRepository.findAll();
   }
 }
 
